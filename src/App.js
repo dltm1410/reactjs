f7789bad40a657d8bd7133af59cfe767cf3c7c27
tuyetmai <duongmai6241@gmail.com>
2024-09-30 20:58:05 +0700

    login page without database

--- src/App.js
+++ src/App.js
@@ -1,11 +1,26 @@
+import React from 'react';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <h1>Hello World</h1>
-      </header>
+    <div className="login-container">
+      <div className="login-box">
+        <h2>Login</h2>
+        <div className="content-container">
+          <div className="input-container">
+            <label>Username</label>
+            <input type="text" placeholder="Enter Username" />
+          </div>
+          <div className="input-container">
+            <label>Password</label>
+            <input type="password" placeholder="Enter Password" />
+          </div>
+          <button className="login-btn" type="submit">Login</button>
+          <div className="register-link">
+            Not registered? <a href="#">Create an account</a>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
